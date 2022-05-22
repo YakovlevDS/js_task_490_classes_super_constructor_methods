d91e06711f690f222b9502a612c60161a1f4f5a7
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-22 16:18:43 +0300

    feat: add  solution 2

--- app.js
+++ app.js
@@ -12,48 +12,80 @@ class Animal {
   }
 
 
-class Shark extends Animal {
+// solution 1
+
+  class Shark extends Animal {
     constructor(name, age, status) {
-      super();
-      this.name = name;
-      this.age = age;
-      this.legs = 0;
-      this.status = status;
-      this.species = 'shark';
+      super(name, age, 0, "shark", status);
     }
   }
   
   class Cat extends Animal {
     constructor(name, age, status) {
-      super();
-      this.name = name;
-      this.age = age;
-      this.status = status;
-      this.legs = 4;
-      this.species = 'cat';
+      super(name, age, 4, "cat", status);
     }
     introduce() {
-      return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+      return `${super.introduce()}  Meow meow!`;
     }
   }
   
   class Dog extends Animal {
-    constructor(name, age, status,master='') {
-      super();
+    constructor(name, age, status, master) {
+      super(name, age, 4, "dog", status);
       this.master = master;
-      this.name = name;
-      this.age = age;
-      this.status = status;
-      this.legs = 4;
-      this.species = 'dog';
-    }
-    introduce() {
-      return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
     }
     greetMaster() {
       return `Hello ${this.master}`;
     }
   }
+
+
+  // solution 2
+// class Shark extends Animal {
+//     constructor(name, age, status) {
+//       super();
+//       this.name = name;
+//       this.age = age;
+//       this.legs = 0;
+//       this.status = status;
+//       this.species = 'shark';
+//     }
+//   }
+  
+//   class Cat extends Animal {
+//     constructor(name, age, status) {
+//       super();
+//       this.name = name;
+//       this.age = age;
+//       this.status = status;
+//       this.legs = 4;
+//       this.species = 'cat';
+//     }
+//     introduce() {
+//       return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+//     }
+//   }
+  
+//   class Dog extends Animal {
+//     constructor(name, age, status,master='') {
+//       super();
+//       this.master = master;
+//       this.name = name;
+//       this.age = age;
+//       this.status = status;
+//       this.legs = 4;
+//       this.species = 'dog';
+//     }
+//     introduce() {
+//       return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+//     }
+//     greetMaster() {
+//       return `Hello ${this.master}`;
+//     }
+//   }
+
+// solution 3
+
 
 
   let example = new Cat("Example", 10, "Happy");
